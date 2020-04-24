94eb5142ef864ea955c122192be3e36d18e58247
Ben Przysucha <32964703+benTenure@users.noreply.github.com>
2020-04-24 18:14:39 -0400

    Working controller that allows player to move and aim. Jumping, crouching, and gravity coming next. Time to study for exams...

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player_transform;
+    [SerializeField] private float mouse_sensitivity;
+    
+    private float x_rotation = 0;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Mouse controls
+        var mouse_x = Input.GetAxis("Mouse X") * mouse_sensitivity;
+        var mouse_y = Input.GetAxis("Mouse Y") * mouse_sensitivity;
+
+        x_rotation -= mouse_y;
+        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(x_rotation, 0f, 0f);
+        player_transform.Rotate(Vector3.up * mouse_x);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movement_speed;
+
+    private CharacterController player_controller;
+    private Transform player_transform;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player_transform = GetComponent<Transform>();
+        player_controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Movement controls
+        var x_axis = Input.GetAxisRaw("Horizontal");
+        var z_axis = Input.GetAxisRaw("Vertical");
+
+        var movement = (player_transform.right * x_axis) + (player_transform.forward * z_axis);
+
+        player_controller.Move(movement * (movement_speed * Time.deltaTime));
+    }
+}
